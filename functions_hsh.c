d474c5b80d2c550773bf4f6a28ae83849f84e61a
MaoScorpion21 <mao7021@hotmail.com>
2022-04-20 23:10:40 -0500

    Update functions_hsh.c

--- functions_hsh.c
+++ functions_hsh.c
@@ -35,8 +35,9 @@ int (*builtin_func[]) (char **) = {
 };
 
 
-int hsh_num_builtins() {
-  return sizeof(builtin_str) / sizeof(char *);
+int hsh_num_builtins(void)
+{
+	return (sizeof(builtin_str) / sizeof(char *));
 
 }
 
@@ -46,33 +47,38 @@ int hsh_num_builtins() {
 int hsh_cd(char **args)
 {
 
-    if (args[1] == NULL) {
-    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
-  } else {
-    if (chdir(args[1]) != 0) {
-      perror("lsh");
-    }
-  }
-  return 1;
+	if (args[1] == NULL)
+	{
+	    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+	} else
+	{
+	    if (chdir(args[1]) != 0)
+	    {
+		    perror("lsh");
+	    }
+	}
+	return (1);
 
 }
 
 int hsh_help(char **args)
 {
 	int i;
-	if (args[1] == NULL){
-	}
+
+	if (args[1] == NULL)
+
   	printf("simple shell\n");
   	printf("Type program names and arguments, and hit enter.\n");
   	printf("The following are built in:\n");
 
-  	for (i = 0; i < hsh_num_builtins(); i++) {
+  	for (i = 0; i < hsh_num_builtins(); i++)
+	{
     		printf("  %s\n", builtin_str[i]);
   	}
 
   	printf("Use the man command for information on other programs.\n");
   	return 1;
-	
+
 }
 
 int hsh_exit(char **args)
